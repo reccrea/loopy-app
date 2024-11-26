8b0737f9a90ab0e64204defce494ce7888d53910
reccrea_lilin <wellmoon0@163.com>
2024-11-26 10:25:25 +0800

    feat: sentry监控

--- src/plugins/modules/sentry.ts
+++ src/plugins/modules/sentry.ts
@@ -5,7 +5,7 @@ export const setupSentry = (app: App) => {
 	Sentry.init({
 		app,
 		// 确保该地址填写正确，指向搭建的 Sentry 服务器，在 Sentry 后台管理页的配置指南页中有给出
-		dsn: 'https://3aeb332c877772ad0df2a439e43e7fc4@o4508278619176960.ingest.us.sentry.io/4508299930959872',
+		dsn: 'https://70875929cfbc9e2761e2fd9c797eb4e5@o4508278619176960.ingest.us.sentry.io/4508362199465984',
 		integrations: [],
 		// 监控采样率（1.0 表示 100% 采样)
 		tracesSampleRate: 1.0
